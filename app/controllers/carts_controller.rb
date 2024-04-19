458ba5111455ea8ec9cf114e02c48945b1a10b5c
hargunsingh07 <hargunsingh@academic.rrc.ca>
2024-04-18 22:53:53 -0500

    Added breadcrumbs to controllers

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,7 @@ class CartsController < BaseController
   add_breadcrumb "home", :root_path
   def show
     add_breadcrumb "products", products_path
+
     @order = current_order
     @order_items = current_order&.order_items
   end
